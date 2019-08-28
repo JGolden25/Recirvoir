b5e6b1c42bced643323c67f870e7d91690b69b89
Joseph Chretien-Golden <joechre@gmail.com>
2019-08-28 00:53:41 -0500

    port part removed

--- index.js
+++ index.js
@@ -7,4 +7,3 @@ const server = require('./data/api/server');
 // --- Server Port ---
 const port = process.env.PORT || 7777;
 app.listen(port, () => console.log(`\n===${port} is live!===\n`))
-const instance = server.listen(port, () => {console.log(`\n-- Server active on port ${instance.address().port} --\n`)})
