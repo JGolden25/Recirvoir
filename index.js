1f40abc797159c39e8411aef37831373fc35aa6c
Joseph Chretien-Golden <joechre@gmail.com>
2019-08-28 00:48:02 -0500

    app.listen added and port semi colon removed (1)

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ require('dotenv').config();
 const server = require('./data/api/server');
 
 // --- Server Port ---
-const port = process.env.PORT || 7777;;
-
+const port = process.env.PORT || 7777;
+app.listen(port, () => console.log(`\n===${port} is live!===\n`))
 const instance = server.listen(port, () => {console.log(`\n-- Server active on port ${instance.address().port} --\n`)})
