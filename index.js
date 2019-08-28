dade9f776ebcac9e8011f5983f3ab171014919ba
Joseph Chretien-Golden <joechre@gmail.com>
2019-08-28 13:34:23 -0500

    Updated Yarn lockfile

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ require('dotenv').config();
 const server = require('./data/api/server');
 
 // --- Server Port ---
-const port = process.env.PORT || 7777;;
+const port = process.env.PORT || 7777;
 
 const instance = server.listen(port, () => {console.log(`\n-- Server active on port ${instance.address().port} --\n`)})
